d4a54949d8ddeefde6025622106e5de4803cea0f
Vyxie <kitakita@disroot.org>
2025-07-23 21:13:56 +0800

    refactor(stk-seen): use curl library instead of XMLRequest

--- src/lobby/commands/stk_seen.cpp
+++ src/lobby/commands/stk_seen.cpp
@@ -18,36 +18,26 @@
 
 #include "stk_seen.hpp"
 #include "io/xml_node.hpp"
+#include "io/file_manager.hpp"
 #include "lobby/server_lobby_commands.hpp"
 #include "lobby/stk_command.hpp"
 #include "lobby/stk_command_context.hpp"
 #include "network/protocols/server_lobby.hpp"
+#include "network/stk_host.hpp"
+#include "network/stk_peer.hpp"
 #include "network/server_config.hpp"
-#include "online/xml_request.hpp"
-#include "online/request_manager.hpp"
+#include "utils/string_utils.hpp"
 #include <parser/argline_parser.hpp>
+#include <curl/curl.h>
 #include <string>
 
 // ========================================================================
 
-class STKSeenRequest : public Online::XMLRequest {
-    private:
-    std::string addr = ServerConfig::m_ishigami_address;
-    public:
-
-    STKSeenRequest(const std::string& username) : XMLRequest(Online::RequestManager::HTTP_MAX_PRIORITY) {
-        setURL(addr + "/stk-seen");
-        addParameter("username", username);
-    };
-    virtual void afterOperation() {
-        Online::XMLRequest::afterOperation();
-        const XMLNode* result = getXMLData();
-
-        if (!isSuccess()) {
-            Log::error("Ishigami", "Failed to get the STK Seen data.");
-        };
-    }
-};
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
 
 bool StkSeenCommand::execute(nnwcli::CommandExecutorContext* const ctx, void* const data)
 {
@@ -62,67 +52,107 @@ bool StkSeenCommand::execute(nnwcli::CommandExecutorContext* const ctx, void* co
     *parser >> playername;
     parser->parse_finish();
 
+    if (playername.length() < 3)
+    {
+	ctx->write("Player name must be at least 3 characters long");
+	ctx->flush();
+	return false;
+    }
+
     ctx->write("Checking player data...");
     ctx->flush();
 
-    // Note that if the context does not survive up to this point,
-    // this will result in an undefined behavior, since
-    // the command can receive a temporary pointer
-    std::thread([ctx, playername]() {
-        auto request = std::make_shared<STKSeenRequest>(playername);
-        Online::RequestManager::get()->addRequest(request);
+    CURL *curl;
+    CURLcode res;
+    std::string response;
+    curl = curl_easy_init();
 
-        while (!request->isDone())
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    if (!curl)
+    {
+        ctx->write("Error: Failed to initialize HTTP request.\n");
+        ctx->flush();
+        return false;
+    }
 
-        const XMLNode* xml = request->getXMLData();
-        if (request->isSuccess())
+    std::string post_data = "username=" + playername;
+    std::string ishigami_addr = ServerConfig::m_ishigami_address;
+    std::string full_url = ishigami_addr + "/stk-seen";
+    std::string response_string;
+    curl_easy_setopt(curl, CURLOPT_URL, full_url.c_str());
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_data.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
+
+    res = curl_easy_perform(curl);
+    if (res != CURLE_OK)
+    {
+	ctx->nprintf("Error: Request failed: %s\n", 512, curl_easy_strerror(res));
+	curl_easy_cleanup(curl);
+	ctx->flush();
+	return false;
+    }
+
+    long response_code;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+    curl_easy_cleanup(curl);
+
+    XMLNode *xml;
+    xml = file_manager->createXMLTreeFromString(response_string);
+
+    if (!xml)
+    {
+	ctx->write("Error: Unexpected response from server.");
+	ctx->flush();
+	return false;
+    }
+
+    std::string rec_success;
+    bool m_success;
+    m_success = false;
+
+    xml->get("success", &rec_success);
+    m_success = (rec_success == "yes");
+
+    if (!m_success)
+    {
+	std::string api_reason;
+        std::string reason;
+
+        xml->get("info", &api_reason);
+            
+        if (api_reason == "player_not_seen")
         {
-            if (xml)
-            {
-                std::string username, country, server, server_country, date;
-                xml->get("username", &username);
-                xml->get("country", &country);
-                xml->get("server", &server);
-                xml->get("server-country", &server_country);
-                xml->get("date", &date);
-
-                ctx->nprintf("Player %s (%s) was last seen on server %s (%s) at %s", 512,
-                    username.c_str(), country.c_str(), server.c_str(),
-                    server_country.c_str(), date.c_str());
-                ctx->flush();
-            }
+            reason = StringUtils::insertValues("Player %s has not been seen on any server recently.", playername);
         }
+        else if (api_reason == "sql_error")
+        {
+            reason = "SQL query failed. Please contact the administrator";
+        }
+	else if (api_reason == "optout")
+	{
+	    reason = "This player has opted out of this feature";
+	}
         else
         {
-            std::string api_reason;
-            std::string reason;
-
-            if (xml)
-                xml->get("info", &api_reason);
-            
-            if (api_reason == "player_not_seen")
-            {
-                reason = StringUtils::insertValues("Player %s has not been seen on any server recently.", playername);
-            }
-            else if (api_reason == "name_too_short")
-            {
-                reason = "Username must be at least 3 characters";
-            }
-            else if (api_reason == "sql_error")
-            {
-                reason = "SQL query failed. Please contact the administrator";
-            }
-            else
-            {
-                reason = "Unspecified error";
-            }
-
-            ctx->write("Failed to get player data: ");
-            ctx->write(reason);
-            ctx->flush();
+            reason = "Unspecified error";
         }
-    }).detach();
 
+	ctx->write("Failed to get player data: ");
+        ctx->write(reason);
+        ctx->flush();
+	return false;
+    }
+
+    std::string username, country, server, server_country, date;
+    xml->get("username", &username);
+    xml->get("country", &country);
+    xml->get("server", &server);
+    xml->get("server-country", &server_country);
+    xml->get("date", &date);
+    ctx->nprintf("Player %s (%s) was last seen on server %s (%s) at %s", 512,
+                    username.c_str(), country.c_str(), server.c_str(),
+                    server_country.c_str(), date.c_str());
+    ctx->flush();
     return true;
 }
